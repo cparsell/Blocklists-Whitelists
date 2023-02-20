25a846df1f101b8cd89bdab898d17aedd21cb1e7
cparsell <cparsell@berkeley.edu>
2023-02-19 19:55:08 -0800

    update python scripts

--- import-whitelist-into-pihole.py
+++ import-whitelist-into-pihole.py
@@ -1,6 +1,13 @@
 # only for importing whitelist txt file into Pihole 
 # not using Docker
 
+# make sure whitelist-nocomments.txt is in same folder as this py file
+# Make sure there are no comments in whitelist file
+# Make sure lines dont start with * or . characters
+# adobe.com 
+# not .adobe.com
+# not *.adobe.com
+
 import subprocess
 whitelist_file='whitelist-nocomments.txt'
 
